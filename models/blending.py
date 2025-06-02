aea99fcd5b98388022ca42e282021ff602e867f9
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-06-02 07:32:50 +0530

    blending

--- models/blending.py
+++ models/blending.py
@@ -10,3 +10,15 @@ def blend_images(img1_path, img2_path):
     height, width = min(A.shape[0], B.shape[0]), min(A.shape[1], B.shape[1])
     A = cv2.resize(A, (width, height))
     B = cv2.resize(B, (width, height))
+    # Generate Gaussian pyramids
+    G = A.copy()
+    gpA = [G]
+    for _ in range(6):
+        G = cv2.pyrDown(G)
+        gpA.append(G)
+
+    G = B.copy()
+    gpB = [G]
+    for _ in range(6):
+        G = cv2.pyrDown(G)
+        gpB.append(G)
