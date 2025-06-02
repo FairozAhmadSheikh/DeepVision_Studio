ea8ee9eb586a9f9718801fe10d8a5fdff0f90a46
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-06-02 07:31:44 +0530

    blending

---
+++ models/blending.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+import os
+
+def blend_images(img1_path, img2_path):
+    A = cv2.imread(img1_path)
+    B = cv2.imread(img2_path)
+
+    # Resize both images to the same size
+    height, width = min(A.shape[0], B.shape[0]), min(A.shape[1], B.shape[1])
+    A = cv2.resize(A, (width, height))
+    B = cv2.resize(B, (width, height))
