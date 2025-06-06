095c4edd82f7957f748913f866c9239a0aed950c
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-06-06 08:14:51 +0530

    c3

--- models/segmenter.py
+++ models/segmenter.py
@@ -9,3 +9,10 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Load pre-trained DeepLabV3
 model = models.segmentation.deeplabv3_resnet101(pretrained=True).eval().to(device)
+# Standard preprocessing
+transform = T.Compose([
+    T.Resize(520),
+    T.ToTensor(),
+    T.Normalize(mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225])
+])
