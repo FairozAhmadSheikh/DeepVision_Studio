b490177cceb4aa19e2e20b1d80e8727c26901d33
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-06-06 08:14:29 +0530

    c2

--- models/segmenter.py
+++ models/segmenter.py
@@ -5,3 +5,7 @@ import cv2
 from PIL import Image
 import os
 import numpy as np
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Load pre-trained DeepLabV3
+model = models.segmentation.deeplabv3_resnet101(pretrained=True).eval().to(device)
