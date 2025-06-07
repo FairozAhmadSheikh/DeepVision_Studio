702d2d5445d518e37b0f9034e4b2a1827a21757c
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-06-07 13:04:23 +0530

    SRGAN

--- models/srgan.py
+++ models/srgan.py
@@ -5,3 +5,21 @@ from PIL import Image
 import os
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# Generator model (from original SRGAN paper)
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.main = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=9, stride=1, padding=4),
+            nn.PReLU(),
+            *[ResidualBlock(64) for _ in range(5)],
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            UpsampleBlock(64),
+            UpsampleBlock(64),
+            nn.Conv2d(64, 3, kernel_size=9, stride=1, padding=4),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        return self.main(x)
