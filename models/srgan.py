8e78cf695e02ce74102c38d9061afc37f030151b
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-06-07 13:04:36 +0530

    SRGAN

--- models/srgan.py
+++ models/srgan.py
@@ -23,3 +23,16 @@ class Generator(nn.Module):
 
     def forward(self, x):
         return self.main(x)
+class ResidualBlock(nn.Module):
+    def __init__(self, channels):
+        super(ResidualBlock, self).__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(channels, channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(channels),
+            nn.PReLU(),
+            nn.Conv2d(channels, channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(channels)
+        )
+
+    def forward(self, x):
+        return x + self.block(x)
